b6ee3cd1d39c4104dd03843f7f6c986ff1c2617d
Aleksandr <mail.bolshakov@gmail.com>
2020-07-31 19:07:48 +0300

    init stage-2 DTO Transactions Service with Repo for Transactions REST Api for Transactions Mock data for transactions

---
+++ src/main/java/pro/bolshakov/geekbrains/springrestapi/service/TransactionServiceImpl.java
@@ -0,0 +1,67 @@
+package pro.bolshakov.geekbrains.springrestapi.service;
+
+import org.springframework.stereotype.Service;
+import pro.bolshakov.geekbrains.springrestapi.dto.TransactionDto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+    private long ind = 1;
+    private Map<Long, TransactionDto> repository = new LinkedHashMap<>();
+
+    @Override
+    public List<TransactionDto> getAll() {
+        return new ArrayList<>(repository.values());
+    }
+
+    @Override
+    public List<TransactionDto> getByPeriod(LocalDate start, LocalDate end) {
+        return repository.values()
+                .stream()
+                .filter(entity -> dateBetween(entity.getDate(), start, end))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<TransactionDto> getIncomes() {
+        return repository.values().stream()
+                .filter(entity -> !entity.isExpense())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<TransactionDto> getExpenses() {
+        return repository.values().stream()
+                .filter(TransactionDto::isExpense)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TransactionDto findById(long id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public void save(TransactionDto dto) {
+        if(dto.getId() == null){
+            dto.setId(ind++);
+        }
+        repository.put(dto.getId(), dto);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        repository.remove(id);
+    }
+
+    private boolean dateBetween(LocalDate date, LocalDate start, LocalDate end){
+        return !(date.isBefore(start) || date.isAfter(end));
+    }
+}
